e97c9a4284a98938ed9703427a2898ffba3eaca9
Abshar777 <absharameen625@gmail.com>
2026-04-20 16:20:53 +0530

    feat: comment out Mentors component in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <Features />
         <Benifits />
         <Courses />
-        <Mentors />
+        {/* <Mentors /> */}
         <AwardsSection />
         <AwwardListing />
         <ContactForm />
